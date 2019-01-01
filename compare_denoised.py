35ed47ec912b650e50082fdd4a0fa5c31281772b
Scott Burwell <scott.j.burwell@gmail.com>
2019-01-01 08:42:54 -0600

    cleaned up compare_denoised.py fewer manual inputs, still in progress...

--- compare_denoised.py
+++ compare_denoised.py
@@ -3,8 +3,12 @@ import glob
 import numpy as np
 import pandas as pd
 import scipy as sp
+from nibabel import load
+from nipype.utils import NUMPY_MMAP                 #may only work on certain systems??
 from nilearn import plotting #Needs at least this version: pip install nilearn==0.5.0a0 (cf. https://nilearn.github.io/whats_new.html)
 
+#these will be required inputs
+bidsdir   = '/labs/mctfr-fmri/bids/es'
 denoisedir= '/labs/burwellstudy/data/fmri/fmriprep-es2/fmriprep/denoised'
 pipelines = [
 '00P',
@@ -15,29 +19,51 @@ pipelines = [
 '09P',
 '24P',
 '36P',
-'03P+SpkRegFD-p2261',
-'03P+SpkRegFD-p2496',
-'09P+SpkRegFD-p2261',
-'09P+SpkRegFD-p2496',
-'36P+SpkRegFD-p2261',
-'36P+SpkRegFD-p2496',
-'AROMANonAgg',
-'AROMANonAgg+01P',
-'AROMANonAgg+02P',
-'AROMANonAgg+03P']
-atlas     = '/labs/burwellstudy/data/rois/Gordon2016+HarvOxSubCort.nii' #349 nodes
-atlasis4d = False
+'03P+SpkReg80thPctile',
+'09P+SpkReg80thPctile',
+'36P+SpkReg80thPctile',
+'00P+aCompCor',
+'24P+aCompCor',
+'24P+aCompCor+4GSR',
+'00P+AROMANonAgg',
+'01P+AROMANonAgg',
+'02P+AROMANonAgg',
+'03P+AROMANonAgg',
+'00P+AROMAAgg',
+'01P+AROMAAgg',
+'02P+AROMAAgg',
+'03P+AROMAAgg']
 
-centsv    = '00P'
-pthr      = .001
+#these will be optional inputs
+atlas     = './atlases/Gordon2016+HarvOxSubCort.nii' #should save this to "allsubjdenoised" from fmriprep2denoised_atlas.py 349 nodes
+pthr      = .001                                     #p-criterion for quantifying edges (uncorrected)
+
+reconvers =['syngo_MR_B17','syngo_MR_D13D','syngo_MR_E11'] #Trio, ESP, ESQ (respectively)
+centsv    = '00P' #or, pipelines[0]
 trpctbad  =   60
-badsubids = '/labs/burwellstudy/projects/twinness/scripts/esmf_poorquality.txt'
+badidlist = '/labs/burwellstudy/projects/twinness/scripts/esmf_poorquality.txt'
 
-subsummary = pd.read_csv(denoisedir + '/' + centsv + '.tsv',sep='\t')
-subs2drop  = list(np.unique(np.concatenate((np.loadtxt(badsubids),
-                  np.array(subsummary.ID[np.where(((subsummary.TRabovethr / subsummary.TR) * 100)>trpctbad)[0]])),axis=0)).astype('int').astype('str')) #kloodge
-subjects   = [i for i in os.listdir(denoisedir) if 'sub-' in i and i.split('sub-')[1] not in subs2drop]
-subid      = np.zeros((len(subjects),1))
+if len(load(atlas, mmap=NUMPY_MMAP).shape)==4:
+   atlasis4d = True
+else:
+   atlasis4d = False
+
+#this section currently not very well-coded or transparent, should redo
+allsubjbids     = pd.read_csv(bidsdir+'/participants.tsv','\t')
+allsubjdenoised = pd.read_csv(denoisedir + '/' + centsv + '.tsv',sep='\t')    #should just include whole sub-???? string
+mapidx          = list()
+for ii in range(0, len(allsubjdenoised.participant_id)):
+    if np.ravel(np.where(allsubjdenoised.participant_id[ii]==allsubjbids.participant_id))[0]:
+       mapidx.append(np.ravel(np.where(allsubjdenoised.participant_id[ii]==allsubjbids.participant_id))[0])
+allsubjbids     = allsubjbids.reindex(np.array(mapidx))
+allsubjdenoised = allsubjdenoised.join( allsubjbids.iloc[:,1:].reset_index(drop=True) )
+subj2drop       = (allsubjdenoised.participant_id[ ((allsubjdenoised.TRabovethr / allsubjdenoised.TR) * 100)>trpctbad ].tolist() + 
+                   open(badidlist).read().split() + 
+                   allsubjdenoised.participant_id[ np.in1d(allsubjdenoised.SoftwareVersions,reconvers)==False ].tolist())
+
+#resume...
+subjects   = [i for i in os.listdir(denoisedir) if 'sub-' in i and i not in subj2drop]
+idlist     = np.chararray(len(subjects),itemsize=len(subjects[0]),unicode=True)
 graphs     = np.zeros((len(subjects),
                      np.loadtxt(glob.glob(denoisedir + '/' + subjects[0] + '/*' + 'Proc-' + pipelines[0] + '_' + 'ROI-' + os.path.basename(atlas).split('.')[0] + '*.tsv')[0]).shape[1],
                      np.loadtxt(glob.glob(denoisedir + '/' + subjects[0] + '/*' + 'Proc-' + pipelines[0] + '_' + 'ROI-' + os.path.basename(atlas).split('.')[0] + '*.tsv')[0]).shape[1]))
@@ -51,10 +77,10 @@ tdof_lsm   = np.zeros(len(pipelines))
 tdof_lsv   = np.zeros(len(pipelines))
 for ii in range(0,len(pipelines)):
     print('Currently on: '+pipelines[ii])
-    subsummary = pd.read_csv(denoisedir + '/' + pipelines[ii] + '.tsv',sep='\t')
+    allsubjdenoised = pd.read_csv(denoisedir + '/' + pipelines[ii] + '.tsv',sep='\t')
 
     for jj in range(0, len(subjects)):
-        subid[jj] = int(subjects[jj].split('sub-')[1])      #Cache subject's identifier
+        idlist[jj] = subjects[jj]                           #Cache subject's identifier
         roits = np.loadtxt(glob.glob(denoisedir+'/'+subjects[jj]+'/*'+'Proc-'+pipelines[ii]+'_'+'ROI-'+os.path.basename(atlas).split('.')[0]+'*.tsv')[0])
         roicm = np.corrcoef(roits.transpose())              #Correlation coeff among ROI time-series
         roicm[roicm==1] = .999                              #The next step can't handle ones, so kloodge
@@ -64,9 +90,9 @@ for ii in range(0,len(pipelines)):
     lowertri = np.where(np.tril(graphs[jj,:,:], -1)!=0)    
 
     ##--- QC-FC association with subject mean FramewiseDisplacement (per Parkes et al., 2018; NeuroImage)
-    allfd = np.zeros(len(subid))
-    for jj in range(0,len(subid)):
-        allfd[jj] = subsummary.meanFD[np.where(subsummary.ID==subid[jj][0])[0][0]]
+    allfd = np.zeros(len(idlist))
+    for jj in range(0,len(idlist)):
+        allfd[jj] = allsubjdenoised.meanFD[np.where(allsubjdenoised.participant_id==idlist[jj])[0]]
     for jj in range(0, len(lowertri[0])):
         pearsonobj  = sp.stats.pearsonr(allfd,graphs[:,lowertri[0][jj],lowertri[1][jj]])
         qcfc_fdr[ii,lowertri[0][jj],lowertri[1][jj]] = pearsonobj[0]
@@ -93,9 +119,9 @@ for ii in range(0,len(pipelines)):
     qcfc_ddr[ii] = spearmanobj.correlation
     qcfc_ddp[ii] = spearmanobj.pvalue
 
-    ##--- QC-FC association with 
-    tdof_lsm[ii] = np.mean(subsummary.PctDFlost) * 100
-    tdof_lsv[ii] = np.std(subsummary.PctDFlost)  * 100 
+    ##--- QC-FC association with temporal degrees of freedom loss
+    tdof_lsm[ii] = np.mean(allsubjdenoised.PctDFlost) * 100
+    tdof_lsv[ii] = np.std(allsubjdenoised.PctDFlost)  * 100 
 
 
 
